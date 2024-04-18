ee9334a7570ef3c9819446e951f2567897c5c12d
Sarthak Dayal <dayalsarthak5@gmail.com>
2024-04-18 16:10:13 -0500

    Include display code from documentation

--- sam.py
+++ sam.py
@@ -2,10 +2,26 @@ from segment_anything import SamPredictor, sam_model_registry, SamAutomaticMaskG
 import matplotlib.pyplot as plt
 import cv2
 
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+    
 def load_image(image_path):
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
     return image
+
 print(1)
 image = load_image("original_image.jpg")
 print(2)
@@ -20,13 +36,8 @@ print(6)
 masks = mask_generator.generate(image)
 print(7)
 
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
+plt.figure(figsize=(20,20))
 plt.imshow(image)
-plt.title('Original Image')
+show_anns(masks)
 plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.imshow(masks, cmap='gray')
-plt.title('Segmented Books')
-plt.axis('off')
+plt.show()
